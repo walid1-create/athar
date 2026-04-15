e399b60f8e3d0cac518bf8cda2145b9591921e79
walidrifaii53@gmail.com <walidrifaii53@gmail.com>
2026-04-15 10:49:18 +0300

    fixed cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://delivery-web-ebon.vercel.app'],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
